a0c33d5dddb16171c82cde19d85dd2ba75620487
Ahmed <88152277+ahmed-skaik@users.noreply.github.com>
2022-08-13 10:28:00 +0300

    first commit, the project isn't realy yet

---
+++ js/pharmacy.js
@@ -0,0 +1,39 @@
+let welcomeMsg = `Hello from JS`
+console.log(welcomeMsg)
+
+// loading screen start
+function loading() {
+  let loader = document.querySelector(".loading")
+  loader.className += " hidden"
+}
+
+window.addEventListener("load", loading)
+
+//to prevent scrolling during loading animation is applied 
+$(window).load(function() {
+  $('body').css({'overflow':'auto', 'height':'auto', 'position':'relative'});
+});
+
+// loading screen end 
+
+
+// scroll up start
+let up = document.getElementById("up")
+
+up.onclick = function () {
+  window.scrollTo ({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+
+function scroll() {
+  if (scrollY >= 200) {
+    up.style.right = "12px";
+  }else {
+    up.style.right = "-100px";
+  }
+}
+
+window.addEventListener("scroll", scroll)
+// scroll up end
