2456b3752c74c74ee8b8e7fe27dc53e91e117592
Ahmed <88152277+ahmed-skaik@users.noreply.github.com>
2022-10-11 00:10:46 +0300

    fixing more issues

--- js/pharmacy.js
+++ js/pharmacy.js
@@ -48,25 +48,25 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 // tooltip end 
 
 // prevent hot keys start
-  document.onkeydown = function(e) { 
-    if(event.keyCode == 123) {
-      return false;
-    }
-    if(e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
-      return false
-    }
-    if(e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
-      return false
-    }
-    if(e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
-      return false
-    }
-    if(e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
-      return false
-    }
-  }
+  // document.onkeydown = function(e) { 
+  //   if(event.keyCode == 123) {
+  //     return false;
+  //   }
+  //   if(e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+  //     return false
+  //   }
+  //   if(e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+  //     return false
+  //   }
+  //   if(e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+  //     return false
+  //   }
+  //   if(e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+  //     return false
+  //   }
+  // }
 
-  document.addEventListener("contextmenu", function(e) { 
-    e.preventDefault();
-  })
+  // document.addEventListener("contextmenu", function(e) { 
+  //   e.preventDefault();
+  // })
 // prevent hot keys end 
