b2aad3e642119fdd638f78e162b9a1988748e36e
Ahmed <88152277+ahmed-skaik@users.noreply.github.com>
2022-12-17 15:58:07 +0200

    adding stats section

--- js/pharmacy.js
+++ js/pharmacy.js
@@ -39,6 +39,36 @@ function scroll() {
 window.addEventListener("scroll", scroll)
 // scroll up end
 
+//stats start
+let nums = document.querySelectorAll(".stats .box .number")
+let statsSection = document.querySelector(".stats")
+let started = false;
+
+function startCount (ele) {
+  let goal = ele.dataset.goal;
+  let count = setInterval(() => {
+    ele.textContent++;
+    if (ele.textContent == goal) {
+      clearInterval(count)
+    }
+  }, (3000 / goal));
+}
+
+function stats() {
+  if (window.scrollY >= statsSection.offsetTop - 350) {
+    if (!started) {
+      nums.forEach((num) => {
+        startCount(num)
+      })    
+    }
+    started = true;
+    }
+}
+
+window.addEventListener("scroll", stats)
+
+//stats end 
+
 
 // tooltip start 
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
