79f705052dc75c9e8b93a88ff3680b525c40831d
Ahmed <88152277+ahmed-skaik@users.noreply.github.com>
2023-02-07 08:03:58 +0200

    spelling issue fixed, and adding .editorconfig file

---
+++ js/levels.js
@@ -0,0 +1,18 @@
+// progress start
+let sectionTop = document.querySelector("#progress")
+let spanProg = document.querySelectorAll(".prog-holder .prog span")
+
+function progress() {
+  if (window.scrollY >= sectionTop.offsetTop - 250) {
+    spanProg.forEach ( (span) => {
+      span.style.width = span.dataset.width;
+    })
+  }else {
+    spanProg.forEach ( (span) => {
+      span.style.width = 0;
+    })
+  }
+}
+
+window.addEventListener("scroll", progress)
+// progress end
